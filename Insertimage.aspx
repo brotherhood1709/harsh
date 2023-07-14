22fc77ad4784bc55a2a2f1b62c902e83237e67ae
Parth Sata <111201138+brotherhood1709@users.noreply.github.com>
2023-07-14 09:30:05 +0530

    Add files via upload
    image

---
+++ Insertimage.aspx
@@ -0,0 +1,26 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Insertimage.aspx.cs" Inherits="Defination.Insertimage" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <h1>Image Upload</h1>
+
+            <asp:Label ID="Label1" runat="server" Text="image Upload"></asp:Label><br />
+            <asp:FileUpload ID="FileUpload1" runat="server" /><br />
+
+
+            <asp:Button ID="Button1" runat="server" Text="Submit" OnClick="Button1_Click" />
+        </div>
+
+        <div>
+            <asp:DataList ID="DataList1" runat="server"></asp:DataList><asp:SqlDataSource runat="server" ID="SqlDataSource1"></asp:SqlDataSource>
+        </div>
+    </form>
+</body>
+</html>
---
+++ Insertimage.aspx.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.Sql;
+using System.Data;
+using System.Drawing;
+using System.IO;
+
+
+namespace Defination
+{
+    public partial class Insertimage : System.Web.UI.Page
+    {
+        SqlConnection cn = new SqlConnection("Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=D:\\Parth\\Defination\\Defination\\App_Data\\Images.mdf;Integrated Security=True");
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            if (FileUpload1.HasFile)
+            {
+                FileUpload1.SaveAs(HttpContext.Current.Request.PhysicalApplicationPath + "image/" + FileUpload1.FileName);
+            }
+            string sql = "insert into images values('"+FileUpload1.FileName+"')";
+            SqlDataAdapter adapter = new SqlDataAdapter(sql,cn);
+            DataTable dt = new DataTable();
+            adapter.Fill(dt);
+            Response.Write("Inserted");
+
+        }
+    }
+}
